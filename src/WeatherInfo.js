ba3ada5549a131117e6d965d2eb4292981248ecf
michelleann815 <68318965+michelleann815@users.noreply.github.com>
2022-04-03 17:09:42 +0900

    Creates WeatherInfo.css, imports it into WeatherInfo.js, makes font size of footer smaller

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -2,21 +2,29 @@ import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
 import WeatherTemperature from "./WeatherTemperature";
+import "./WeatherInfo.css";
 
-export default function WeatherInfo (props) {
-    return (
-<div className="WeatherInfo">
-    <div className="cityTime">
-      <h1 className="city">{props.data.city}</h1>
-      <h5 className="date"><FormattedDate date={props.data.date} /></h5>
+export default function WeatherInfo(props) {
+  return (
+    <div className="WeatherInfo">
+      <div className="cityTime">
+        <h1 className="city">{props.data.city}</h1>
+        <h5 className="date">
+          <FormattedDate date={props.data.date} />
+        </h5>
+      </div>
+      <div className="windHumidityDescription">
+        <p className="wind"> {props.data.wind} km/h winds</p>
+        <p className="humidity">{props.data.humidity}% humidity</p>
+        <p className="description">{props.data.description}</p>
+      </div>
+      <WeatherIcon
+        code={props.data.icon}
+        alt={props.data.description}
+        width="115px"
+        size={74}
+      />
+      <WeatherTemperature celsius={props.data.temperature} />
     </div>
-    <div className="windHumidityDescription">
-      <p className="wind"> {props.data.wind} km/h winds</p>
-      <p className="humidity">{props.data.humidity}% humidity</p>
-      <p className="description">{props.data.description}</p>
-    </div>
-    <WeatherIcon code={props.data.icon} alt={props.data.description} width="115px" size={74}/>
-    <WeatherTemperature celsius={props.data.temperature}/>
-</div>
-    )
+  );
 }
